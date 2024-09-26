c4cb8b401242dfde0d39980e969a330ba0cc2dbd
DanM <83934559+danmancs@users.noreply.github.com>
2024-09-26 14:21:50 +1000

    new button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,10 +14,10 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
-                  href="#about"
+                  href="https://cal.com/dan-procurable.group/meet"
                   className="btn btn-custom btn-lg page-scroll"
                 >
-                  Learn More
+                  Book a Meeting
                 </a>{" "}
               </div>
             </div>
